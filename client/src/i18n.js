38808c0e9fc15ad7e466ea8dc6c3fbdb3bbdb06c
Erij Maherzia BEN BRAHIM <erijmaherziabenbrahim@gmail.com>
2022-09-21 22:06:18 +0100

    Task 1 and 2 Done

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,9 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-
-
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(Backend)
@@ -13,8 +11,7 @@ i18n
     fallbackLng: "en",
 
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
-
-  export default i18n;
+export default i18n;
